e0cc869708f915a81e0a0c7f07a740b9c0e010eb
GustavoBorges <sgustavo.borges10@gmail.com>
2021-06-07 10:56:12 -0300

    Usuario possivelmente finalizado

--- Classes/Usuario.cs
+++ Classes/Usuario.cs
@@ -1,18 +1,28 @@
 using System.Collections.Generic;
 using AulaPOO_ProjetoDeProdutos.Interfaces;
+using System;
 
 namespace AulaPOO_ProjetoDeProdutos.Classes
 {
     public class Usuario : IUsuario
     {
+        public int Codigo{get; set;}
+        public string Nome{get; set;}
+        public string Email{get; set;}
+        public string Senha{get; set;}
+        public DateTime DataCadastro{get; set;}
+
+        List<Usuario> users = new List<Usuario>();
         public string Cadastrar(Usuario userCadastrar)
         {
-            return ""; //para não dar erro de return
+            users.Add(userCadastrar);
+            return "Usuário cadastrado"; 
         }
 
         public string Deletar(Usuario userDeletar)
         {
-            return ""; //para não dar erro de return
+            users.Remove(userDeletar);
+            return "Usuário deletado"; 
         }
     }
 }
